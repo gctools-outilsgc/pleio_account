8ec11eae7d99a3df9b5a77c7a4129ec4836323ed
Mark Wooff <mark.wooff@gmail.com>
2017-12-19 10:22:43 -0500

    Added OpenID package

--- pleio_account/settings.py
+++ pleio_account/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django_otp.plugins.otp_static',
     'django_otp.plugins.otp_totp',
     'two_factor',
+    'oidc_provider'
 ]
 
 REST_FRAMEWORK = {
@@ -162,7 +163,8 @@ GEOIP_PATH = os.path.join(BASE_DIR, 'assets/geopip2/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-LOGIN_URL = 'two_factor:login'
+LOGIN_URL = '/accounts/login/'
+# LOGIN_URL = 'two_factor:login'
 LOGIN_REDIRECT_URL = '/profile/'
 LOGOUT_REDIRECT_URL = 'two_factor:login'
 
