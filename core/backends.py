9c769112e704a92c41e5b5fff1b6f817bb3ad21e
Mark Wooff <mark.wooff@gmail.com>
2018-02-19 13:18:03 -0500

    Empty array instead of False

--- core/backends.py
+++ core/backends.py
@@ -19,7 +19,7 @@ class ElggBackend:
         # Verify username/password combination
         valid_pass_request = requests.post(elgg_url + "/services/api/rest/json/", data={'method': 'pleio.verifyuser', 'user': username, 'password': password})
         valid_pass_json = json.loads(valid_pass_request.text)
-        valid_pass_result = valid_pass_json["result"] if 'result' in valid_pass_json else False
+        valid_pass_result = valid_pass_json["result"] if 'result' in valid_pass_json else []
         valid_pass = valid_pass_result["valid"] if 'valid' in valid_pass_result else False
         name = valid_pass_result["name"] if 'name' in valid_pass_result else username
         admin = valid_pass_result["admin"] if 'admin' in valid_pass_result else False
