11186a37ec68df60b39d8c1825a776d436c0635c
ethanWallace <ethan.wallace.91@gmail.com>
2018-04-04 10:58:13 -0400

    Added two step form with back link

--- requestmembership/forms.py
+++ requestmembership/forms.py
@@ -1,1 +1,35 @@
 from django import forms
+from django.utils.translation import gettext, gettext_lazy as _
+from core.models import User
+from core.forms import EmailField
+from emailvalidator.validator import is_email_valid
+
+class RequestStepOne(forms.Form):
+    name = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'aria-labelledby':"error_name"}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'aria-labelledby':"error_email"}))
+
+    def clean_email(self):
+        # Get the emails
+        email = self.cleaned_data.get('email')
+        try:
+            match = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return email
+
+        raise forms.ValidationError(self.error_messages['unique_email'], code='unique_email',)
+
+class RequestStepTwo(forms.Form):
+    name = forms.CharField(required=True, max_length=100, widget=forms.TextInput(attrs={'aria-labelledby':"error_name"}))
+    email = forms.CharField(required=True, widget=forms.TextInput(attrs={'aria-labelledby':"error_email"}))
+    number = forms.CharField(required=True, max_length=10)
+    street = forms.CharField(required=True, max_length=100)
+    city = forms.CharField(required=True, max_length=100)
+    province = forms.CharField(required=True, max_length=3)
+    country = forms.CharField(required=True, max_length=30)
+    postal = forms.CharField(required=True, max_length=6)
+
+    reason = forms.CharField(required=True, max_length=280, widget=forms.Textarea(attrs={'rows':4, 'cols':40}))
+    website = forms.URLField(required=True)
+    password1 = forms.CharField(strip=False, widget=forms.PasswordInput(attrs={'aria-labelledby':"error_password1", "aria-describedby":"password_help"}))
+    password2 = forms.CharField(strip=False, widget=forms.PasswordInput(attrs={'aria-labelledby':"error_password2"}))
+    accepted_terms = forms.BooleanField(required=True)
--- requestmembership/views.py
+++ requestmembership/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import RequestStepOne, RequestStepTwo
 
 def requestmembership_step_one(request):
-    return render(request, 'requestmembership_step_one.html')
+    if request.method == 'POST':
+        form = RequestStepOne(request.POST)
+    else:
+        form = RequestStepOne()
+
+    if 'step_one_form' in request.session:
+        data = request.session['step_one_form']
+        
+        for attr, value in data.items():
+            form.fields[attr].initial = value
+
+        del request.session['step_one_form']
+
+    return render(request, 'requestmembership_step_one.html', {'form': form})
 
 def requestmembership_step_two(request):
-    return render(request, 'requestmembership_step_two.html')
+    if request.method == 'POST':
+        form = RequestStepOne(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            form2 = RequestStepTwo()
+            request.session['step_one_form'] = data;
+            return render(request, 'requestmembership_step_two.html', {'form': form2, 'data': data})
+        else:
+            return render(request, 'requestmembership_step_one.html', {'form': form})
+    else:
+        return HttpResponseRedirect('/request/one/')
+
+    def clean(self):
+        if 'back' in self.data:
+            form = RequestStepOne(request.POST)
+            return render(request, 'requestmembership_step_one.html', {'form': form})
 
 def requestmembership_complete(request):
     return render(request, 'requestmembership_complete.html')
