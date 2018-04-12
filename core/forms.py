cd260cb41557ad9f883ac96d885350cc081f9ac3
Mark Wooff <mark.wooff@gmail.com>
2018-04-12 02:39:37 -0400

    Missing import

--- core/forms.py
+++ core/forms.py
@@ -13,6 +13,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template import loader
+from django.views.generic.edit import FormView
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
