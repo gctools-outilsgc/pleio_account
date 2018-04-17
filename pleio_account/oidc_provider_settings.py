3594eafca98cc8819946e2cb4df50353ed439a12
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2018-04-17 10:15:09 -0400

    Revert "query camelcase error gcID instead of gcId"
    This reverts commit bb861eb8af3c278eca784ff17d55336ab5b8a631.

--- pleio_account/oidc_provider_settings.py
+++ pleio_account/oidc_provider_settings.py
@@ -14,7 +14,7 @@ def userinfo(claims, user):
     claims['email'] = user.email
 
     # Get rest of information from Profile as a Service
-    query = {'query': 'query{profiles(gcId: "' + str(user.id) + '"){name, email, avatar, mobilePhone, officePhone,' +
+    query = {'query': 'query{profiles(gcID: "' + str(user.id) + '"){name, email, avatar, mobilePhone, officePhone,' +
                       'address{streetAddress,city, province, postalCode, country}}}'}
 
     response = requests.post(settings.GRAPHQL_ENDPOINT, headers={'Authorization': 'Token ' + settings.GRAPHQL_TOKEN},
