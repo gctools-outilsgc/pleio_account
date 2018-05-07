b14a5dfc33cf52c4f03391a591f50330b480fb0a
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2018-05-07 14:06:01 -0400

    Revert "Fix to EmailRegexValidator migration crashing"
    This reverts commit e95acfebc27fbb20e6c952ec188db4b7cb228f84.

--- emailvalidator/migrations/0002_domain_catchall.py
+++ emailvalidator/migrations/0002_domain_catchall.py
@@ -15,11 +15,11 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='EmailRegExValidator',
             name='name',
-            field=models.CharField(max_length=255, verbose_name='Name'),
+            field=models.CharField(blank=True, null=True, max_length=255),
         ),
         migrations.AddField(
             model_name='EmailRegExValidator',
             name='allow_all',
-            field=models.BooleanField(default=False, verbose_name='Allow all from domain'),
+            field=models.BooleanField(blank=True, null=True, default=False),
         ),
     ]
