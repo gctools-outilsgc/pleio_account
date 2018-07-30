44cbf490c7fef97bac2b811d923da47cc5bea31b
ethanWallace <ethan.wallace.91@gmail.com>
2018-07-30 11:22:49 -0400

    Revert "Site Configuration model passes values now"
    This reverts commit 58f34fd8c7015ee9405bff187c6057d522dee3d3.

--- pleio_account/settings.py
+++ pleio_account/settings.py
@@ -31,7 +31,6 @@ WEBPACK_LOADER = {
 # Application definition
 
 INSTALLED_APPS = [
-    'solo',
     'core',
     'emailvalidator',
     'api',
@@ -186,8 +185,6 @@ FRESHDESK_URL = 'https://gccollab.gctools-outilsgc.ca/'
 FRESHDESK_SECRET_KEY = os.getenv('FRESHDESK_SECRET_KEY', '')
 
 ELGG_URL = 'https://gccollab.ca'
-
-EMAIL_BACKEND = 'core.backends.SiteConfigEmailBackend'
 
 PASSWORD_RESET_TIMEOUT_DAYS = 1
 ACCOUNT_ACTIVATION_DAYS = 7
