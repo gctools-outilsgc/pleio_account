040fb44b5749e56e2246543a086ddb39b293a24a
steph4104 <stephanie4104@gmail.com>
2019-05-23 11:46:09 -0400

    create seperate dropdown

--- core/forms.py
+++ core/forms.py
@@ -348,27 +348,35 @@ class ChangePasswordForm(forms.Form):
 
 
 class ChooseSecurityQuestion(forms.Form):
-    QUESTIONS = [
+    QUESTIONS1 = [
         ('', _('Please select one of the questions')),
         (1, _('What is your favourite board game?')),
         (2, _('Who is your favourite fictional character?')),
         (3, _('What is your least favourite chore?')),
-        (4, _('What type of music do you dislike most?')),
-        (5, _('What was your favourite TV show when you were a child?')),
-        (6, _('Who was your best friend in kindergarten?')),
-        (7, _(
-            'If you won the lottery, what would be your first big purchase?'
-        )),
-        (8, _('What is the first movie you saw in theatres?')),
-        (9, _('What was your first cell phone?')),
-        (10, _('What movie do you know the most quotes from?'))
     ]
 
-    question_one = forms.ChoiceField(choices=QUESTIONS, initial=0)
+    QUESTIONS2 = [
+        ('', _('Please select one of the questions')),
+        (1, _('What type of music do you dislike most?')),
+        (2, _('What was your favourite TV show when you were a child?')),
+        (3, _('Who was your best friend in kindergarten?')),
+    ]
+
+    QUESTIONS3 = [
+        ('', _('Please select one of the questions')),
+        (1, _(
+            'If you won the lottery, what would be your first big purchase?'
+        )),
+        (2, _('What is the first movie you saw in theatres?')),
+        (3, _('What was your first cell phone?')),
+        (4, _('What movie do you know the most quotes from?'))
+    ]
+
+    question_one = forms.ChoiceField(choices=QUESTIONS1, initial=0)
     answer_one = forms.CharField(min_length=3, max_length=100)
-    question_two = forms.ChoiceField(choices=QUESTIONS, initial=0)
+    question_two = forms.ChoiceField(choices=QUESTIONS2, initial=0)
     answer_two = forms.CharField(min_length=3, max_length=100)
-    question_three = forms.ChoiceField(choices=QUESTIONS, initial=0)
+    question_three = forms.ChoiceField(choices=QUESTIONS3, initial=0)
     answer_three = forms.CharField(min_length=3, max_length=100)
 
     def clean(self):
