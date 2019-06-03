901eb5d7e4f0186e185ac49de87061d8add1b726
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-06-03 14:39:55 -0400

    CORS function to check redirect_uris

--- core/apps.py
+++ core/apps.py
@@ -1,4 +1,9 @@
 from django.apps import AppConfig
 
+
 class CoreConfig(AppConfig):
     name = 'core'
+
+    def ready(self):
+        # Makes sure all signal handlers are connected
+        from core import handler
