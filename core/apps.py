8a0afa367d139af05e38b0cb504c339607577c91
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-06-04 15:39:37 -0400

    Remove CORS check from userinfo endpoint

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     name = 'core'
 
+    def ready(self):
+        # Makes sure all signal handlers are connected
+        from core import handler
