8d1ea8a66cfb0436a043ba8c230c1635ed59dcc3
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-06-04 13:10:26 -0400

    Added middleware for dynamic CORS settings

--- core/apps.py
+++ core/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     name = 'core'
 
-    def ready(self):
-        # Makes sure all signal handlers are connected
-        from core import handler
