4a80f4ccfef57b06e2815b549481599e3cc16c02
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-06-04 15:54:12 -0400

    last piece of the puzzle

--- core/handler.py
+++ core/handler.py
@@ -1,5 +1,7 @@
 from corsheaders.signals import check_request_enabled
 
+# Required until fix implemented in django-oidc-provider
+# to allow for OPTION on preflight request to userinfo
 def cors_allow_api_to_everyone(sender, request, **kwargs):
     return request.path.startswith('/openid/userinfo')
 
