0aef077a948f6db02e8168d11f34a13ad69d7a35
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-06-04 15:39:37 -0400

    Remove CORS check from userinfo endpoint

---
+++ core/handler.py
@@ -0,0 +1,6 @@
+from corsheaders.signals import check_request_enabled
+
+def cors_allow_api_to_everyone(sender, request, **kwargs):
+    return request.path.startswith('/openid/userinfo')
+
+check_request_enabled.connect(cors_allow_api_to_everyone)
