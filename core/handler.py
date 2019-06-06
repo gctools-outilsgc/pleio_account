ab36d3a19d39eece1edf54541bb6fb12282cb6cc
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-06-06 16:16:07 -0400

    adding .well-known path to CORS exception

--- core/handler.py
+++ core/handler.py
@@ -3,6 +3,6 @@ from corsheaders.signals import check_request_enabled
 # Required until fix implemented in django-oidc-provider
 # to allow for OPTION on preflight request to userinfo
 def cors_allow_api_to_everyone(sender, request, **kwargs):
-    return request.path.startswith('/openid/userinfo')
+    return request.path.startswith('/openid/userinfo') or request.path.startswith('openid/.well-known')
 
 check_request_enabled.connect(cors_allow_api_to_everyone)
