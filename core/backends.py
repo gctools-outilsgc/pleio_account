705533d20b9497c07c93dac051cb134958256762
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-06-07 13:06:28 -0400

    Fix email not sending bug

--- core/backends.py
+++ core/backends.py
@@ -74,7 +74,7 @@ class SiteConfigEmailBackend(EmailBackend):
              **kwargs
         )
 
-    def send_messages(self, email_messages):
-        return len(list(email_messages))
+    #def send_messages(self, email_messages):
+    #    return len(list(email_messages))
 
 __all__ = ['SiteConfigEmailBackend']
