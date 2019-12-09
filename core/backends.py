77521e7e1bf677914813311f0a459e3e0c2baba7
ethanWallace <ethan.wallace.91@gmail.com>
2019-12-09 08:22:29 -0500

    Account lockout / Elgg backend Fix (#156)
    * New backend to handle axes login flow
    
    * Codacy review
    
    * Added break to backend
    
    This will stop looping through all urls if a user has been created.

--- core/backends.py
+++ core/backends.py
@@ -4,6 +4,73 @@ from .models import User
 from django.core.mail.backends.smtp import EmailBackend
 from constance import config
 
+from django.core.exceptions import PermissionDenied
+from axes.attempts import is_already_locked
+from axes.utils import get_credentials, get_lockout_message
+from axes.backends import AxesModelBackend
+
+# Combine AxesModelBackend and ElggBackend into one backend
+class ElggLockout(AxesModelBackend):
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+
+        if request is None:
+            raise AxesModelBackend.RequestParameterRequired()
+
+        try:
+            user = User.objects.get(email__iexact=username)
+            if user.check_password(password):
+                return
+        except User.DoesNotExist:
+            if config.ELGG_URL:
+                elgg_urls =  config.ELGG_URL.splitlines()
+
+                for url in elgg_urls:
+                    # Verify username/password combination
+                    valid_user_request = requests.post(
+                        url + "/services/api/rest/json/",
+                        data={
+                            'method': 'pleio.verifyuser',
+                            'user': username,
+                            'password': password
+                        }
+                    )
+                    valid_user_json = json.loads(valid_user_request.text)
+                    valid_user_result = valid_user_json["result"] if 'result' in valid_user_json else []
+                    valid_user = valid_user_result["valid"] if 'valid' in valid_user_result else False
+                    name = valid_user_result["name"] if 'name' in valid_user_result else username
+                    admin = valid_user_result["admin"] if 'admin' in valid_user_result else False
+
+                    # If valid, create new user with Elgg attributes
+                    if valid_user is True:
+                        user = User.objects.create_user(
+                            name=name,
+                            email=username.lower(),
+                            password=password,
+                            accepted_terms=True,
+                            receives_newsletter=True
+                        )
+                        user.is_active = True
+                        user.is_admin = admin
+                        user.save()
+                        break
+
+        credentials = get_credentials(username=username, password=password, **kwargs)
+
+        if is_already_locked(request, credentials):
+            error_msg = get_lockout_message()
+            response_context = kwargs.get('response_context', {})
+            response_context['error'] = error_msg
+            raise PermissionDenied(error_msg)
+
+        return
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
 
 class ElggBackend:
     def authenticate(self, request, username=None, password=None):
