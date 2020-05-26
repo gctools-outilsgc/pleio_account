8a4654b3f19bbe41118f971a78e50e999dd2b052
Ethan Wallace <ethan.wallace.91@gmail.com>
2020-05-26 12:11:54 -0500

    Creates a SM exchange on startup

--- core/apps.py
+++ core/apps.py
@@ -1,4 +1,6 @@
 from django.apps import AppConfig
+import pika
+from constance import config
 
 
 class CoreConfig(AppConfig):
@@ -7,3 +9,12 @@ class CoreConfig(AppConfig):
     def ready(self):
         # Makes sure all signal handlers are connected
         from core import handler
+
+
+        if config.SERVICE_MESH_ACTIVATION:
+            credentials = pika.PlainCredentials(config.SERVICE_MESH_USER, config.SERVICE_MESH_PASSWORD)
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.SERVICE_MESH_HOST, credentials=credentials))
+            channel = connection.channel()
+            channel.exchange_declare(exchange='account', exchange_type='topic', durable=True)
+
+            connection.close()
